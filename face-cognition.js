1a9d9ef43a6c64878b362522604713bf0b8908c9
Yang <zyang1@illumina.com>
2020-01-15 09:43:29 -0800

    add new image urls
    add new image url to js

---
+++ face-cognition.js
@@ -0,0 +1,50 @@
+
+
+//Global Variables
+
+
+//Images
+const NEW_1 = "https://raw.githubusercontent.com/yoshi0925/face-recognition/master/Images/new1.JPG";
+const NEW_2 = "https://raw.githubusercontent.com/yoshi0925/face-recognition/master/Images/new2.JPG";
+const NEW_3 = "https://raw.githubusercontent.com/yoshi0925/face-recognition/master/Images/new3.JPG";
+const NEW_4 = "https://raw.githubusercontent.com/yoshi0925/face-recognition/master/Images/new4.JPG";
+const NEW_5 = "https://raw.githubusercontent.com/yoshi0925/face-recognition/master/Images/new5.JPG";
+const NEW_6 = "https://raw.githubusercontent.com/yoshi0925/face-recognition/master/Images/new6.JPG";
+const NEW_7 = "https://raw.githubusercontent.com/yoshi0925/face-recognition/master/Images/new7.JPG";
+const NEW_8 = "https://raw.githubusercontent.com/yoshi0925/face-recognition/master/Images/new8.JPG";
+const NEW_9 = "https://raw.githubusercontent.com/yoshi0925/face-recognition/master/Images/new9.JPG";
+const NEW_10 = "https://raw.githubusercontent.com/yoshi0925/face-recognition/master/Images/new10.JPG";
+const NEW_11 = "https://raw.githubusercontent.com/yoshi0925/face-recognition/master/Images/new11.JPG";
+const NEW_12 = "https://raw.githubusercontent.com/yoshi0925/face-recognition/master/Images/new12.JPG";
+const NEW_13 = "https://raw.githubusercontent.com/yoshi0925/face-recognition/master/Images/new13.JPG";
+const NEW_14 = "https://raw.githubusercontent.com/yoshi0925/face-recognition/master/Images/new14.JPG";
+const NEW_15 = "https://raw.githubusercontent.com/yoshi0925/face-recognition/master/Images/new15.JPG";
+const NEW_16 = "https://raw.githubusercontent.com/yoshi0925/face-recognition/master/Images/new16.JPG";
+const NEW_17 = "https://raw.githubusercontent.com/yoshi0925/face-recognition/master/Images/new17.JPG";
+const NEW_18 = "https://raw.githubusercontent.com/yoshi0925/face-recognition/master/Images/new18.JPG";
+const NEW_19 = "https://raw.githubusercontent.com/yoshi0925/face-recognition/master/Images/new19.JPG";
+const NEW_20 = "https://raw.githubusercontent.com/yoshi0925/face-recognition/master/Images/new20.JPG";
+const NEW_21 = "https://raw.githubusercontent.com/yoshi0925/face-recognition/master/Images/new21.JPG";
+const NEW_22 = "https://raw.githubusercontent.com/yoshi0925/face-recognition/master/Images/new22.JPG";
+const OLD_1 = "";
+const OLD_2 = "";
+const OLD_3 = "";
+const OLD_4 = "";
+const OLD_5 = "";
+const OLD_6 = "";
+const OLD_7 = "";
+const OLD_8 = "";
+const OLD_9 = "";
+const OLD_10 = "";
+const OLD_11 = "";
+const OLD_12 = "";
+const OLD_13 = "";
+const OLD_14 = "";
+const OLD_15 = "";
+const OLD_16 = "";
+const OLD_17 = "";
+const OLD_18 = "";
+const OLD_19 = "";
+const OLD_20 = "";
+const OLD_21 = "";
+const OLD_22 = "";
